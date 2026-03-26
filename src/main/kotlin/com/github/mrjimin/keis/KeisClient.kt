e83db60111199aadbdef45c6b7363c81776171e9
Jimin Seo <aa090402@naver.com>
2026-03-26 19:31:29 +0900

    잡종 클라이언트 파일

--- src/main/kotlin/com/github/mrjimin/keis/KeisClient.kt
+++ src/main/kotlin/com/github/mrjimin/keis/KeisClient.kt
@@ -1,13 +1,15 @@
 package com.github.mrjimin.keis
 
-import com.github.mrjimin.keis.enums.KeisAPI
+import com.github.mrjimin.keis.dto.common.KeisWrapper
+import com.github.mrjimin.keis.internal.KeisModule.json
 import io.ktor.client.*
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.*
 
 class KeisClient(
     val config: KeisConfig,
@@ -23,25 +25,97 @@ class KeisClient(
         val defaultClient: HttpClient by lazy {
             HttpClient(CIO) {
                 install(ContentNegotiation) {
-                    register(ContentType.Any, KotlinxSerializationConverter(KeisModule.json))
+                    json(json)
                 }
             }
         }
     }
 
-    suspend inline fun <reified T> request(
-        api: KeisAPI,
-        builder: HttpRequestBuilder.() -> Unit = {}
-    ): T = client.get("$BASE_URL/${api.value}") {
-        accept(ContentType.Any)
+    internal suspend inline fun <reified T> requestRows(
+        endpoint: String,
+        noinline builder: HttpRequestBuilder.() -> Unit = {}
+    ): List<T> {
 
-        parameter("KEY", config.key)
-        parameter("Type", config.type.value)
-        parameter("pIndex", config.pIndex)
-        parameter("pSize", config.pSize)
+        val response = request(endpoint, builder)
 
-        if (config.stats) parameter("stats", "true")
+        val wrappers = json.decodeFromJsonElement<List<KeisWrapper<T>>>(
+            response[endpoint] ?: return emptyList()
+        )
 
-        builder()
-    }.body()
+        return wrappers.flatMap { it.row ?: emptyList() }
+    }
+
+    inline fun <reified T> JsonObject.parseRows(endpoint: String): List<T> {
+        val wrappers = json.decodeFromJsonElement<List<KeisWrapper<T>>>(
+            this[endpoint] ?: return emptyList()
+        )
+
+        return wrappers.flatMap { it.row ?: emptyList() }
+    }
+
+//
+//    internal suspend inline fun <reified T : KeisEntity> requestRows(
+//        endpoint: String,
+//        noinline builder: HttpRequestBuilder.() -> Unit = {}
+//    ): List<T> {
+//        val response = request(endpoint, builder)
+//
+//        val rows = response[endpoint]
+//            ?.jsonArray
+//            ?.getOrNull(1)
+//            ?.jsonObject
+//            ?.get("row")
+//            ?.jsonArray
+//            ?: return emptyList()
+//
+//        return rows.mapNotNull { element ->
+//            val obj = element as? JsonObject ?: return@mapNotNull null
+//
+//            json.decodeFromJsonElement<T>(obj).apply {
+//                client = this@KeisClient
+//                json = obj
+//            }
+//        }
+//    }
+
+//
+    private suspend fun request(
+        endpoint: String,
+        builder: HttpRequestBuilder.() -> Unit
+    ): JsonObject {
+        return client.get("$BASE_URL/$endpoint") {
+            accept(ContentType.Any)
+
+            parameter("KEY", config.key)
+            parameter("Type", config.type)
+            parameter("pIndex", config.pIndex)
+            parameter("pSize", config.pSize)
+
+            if (config.stats) parameter("stats", "true")
+
+            builder()
+        }.body()
+    }
+//
+//    private fun JsonObject.extractRows(endpoint: String): JsonArray? {
+//        return this[endpoint]
+//            ?.jsonArray
+//            ?.getOrNull(1)
+//            ?.jsonObject
+//            ?.get("row")
+//            ?.jsonArray
+//    }
+//
+//    private inline fun <reified T : KeisEntity> decodeRows(
+//        rows: JsonArray
+//    ): List<T> {
+//        return rows.mapNotNull { element ->
+//            val obj = element as? JsonObject ?: return@mapNotNull null
+//
+//            json.decodeFromJsonElement<T>(obj).apply {
+//                client = this@KeisClient
+//                json = obj
+//            }
+//        }
+//    }
 }
