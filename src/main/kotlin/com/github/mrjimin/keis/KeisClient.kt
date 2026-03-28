81397a6c9f7c54f434c9e8d6f2ca1513f8c87d5c
Jimin Seo <aa090402@naver.com>
2026-03-29 01:53:22 +0900

    가시성 변경

--- src/main/kotlin/com/github/mrjimin/keis/KeisClient.kt
+++ src/main/kotlin/com/github/mrjimin/keis/KeisClient.kt
@@ -10,15 +10,25 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.decodeFromJsonElement
 
 class KeisClient(
-    val config: KeisConfig,
-    val httpClient: HttpClient = defaultClient,
+    private val config: KeisConfig,
+    private val httpClient: HttpClient = defaultClient,
 ) {
 
-    constructor(key: String) : this(
-        config = KeisConfig(key)
-    )
+    constructor(key: String) : this(KeisConfig(key))
 
-    suspend fun getJson(endpoint: String, block: HttpRequestBuilder.() -> Unit = {}): JsonObject {
+    suspend inline fun <reified T> requestRows(
+        endpoint: String,
+        noinline block: HttpRequestBuilder.() -> Unit = {}
+    ): List<T> {
+        val jsonObject = getJson(endpoint, block)
+        return jsonObject.parseRows(endpoint)
+    }
+
+    @PublishedApi
+    internal suspend fun getJson(
+        endpoint: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+    ): JsonObject {
         return httpClient.get("https://open.neis.go.kr/hub/$endpoint") {
             accept(ContentType.Any)
 
@@ -33,17 +43,11 @@ class KeisClient(
         }.body()
     }
 
-    suspend inline fun <reified T> requestRows(
-        endpoint: String,
-        noinline block: HttpRequestBuilder.() -> Unit = {}
-    ): List<T> {
-        val jsonObject = getJson(endpoint, block)
-        return jsonObject.parseRows(endpoint)
-    }
-
-    inline fun <reified T> JsonObject.parseRows(endpoint: String): List<T> {
-        val wrappers = this[endpoint]?.let { json.decodeFromJsonElement<List<KeisWrapper<T>>>(it) }
-            ?: return emptyList()
+    @PublishedApi
+    internal inline fun <reified T> JsonObject.parseRows(endpoint: String): List<T> {
+        val wrappers = this[endpoint]?.let {
+            json.decodeFromJsonElement<List<KeisWrapper<T>>>(it)
+        } ?: return emptyList()
 
         return wrappers.flatMap { it.row ?: emptyList() }
     }
