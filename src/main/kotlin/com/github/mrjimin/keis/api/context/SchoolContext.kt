8527882040c0f065522f36e120d5c8c821e18b31
Jimin Seo <aa090402@naver.com>
2026-03-29 02:56:48 +0900

    feat: SchoolContext.kt 수정

--- src/main/kotlin/com/github/mrjimin/keis/api/context/SchoolContext.kt
+++ src/main/kotlin/com/github/mrjimin/keis/api/context/SchoolContext.kt
@@ -2,11 +2,9 @@ package com.github.mrjimin.keis.api.context
 
 import com.github.mrjimin.keis.KeisClient
 import com.github.mrjimin.keis.api.dsl.TimetableQueryBuilder
-import com.github.mrjimin.keis.api.*
-import com.github.mrjimin.keis.internal.*
+import com.github.mrjimin.keis.api.timetable
 import com.github.mrjimin.keis.model.domain.School
 import com.github.mrjimin.keis.model.domain.Timetable
-import java.time.LocalDate
 
 class SchoolContext(
     private val client: KeisClient,
@@ -19,26 +17,6 @@ class SchoolContext(
         return client.timetable(school, block)
     }
 
-    suspend fun timetableOf(
-        from: LocalDate = startOfWeek(),
-        to: LocalDate = endOfWeek(),
-        grade: Int? = null,
-        classNumber: Int? = null,
-        major: String? = null,
-        fillMissing: Boolean = false,
-        maxPeriod: Int = school.type.defaultMaxPeriod
-    ): List<Timetable> {
-        return client.timetableOf(
-            school,
-            from,
-            to,
-            grade,
-            classNumber,
-            major,
-            fillMissing,
-            maxPeriod
-        )
-    }
 }
 
 fun School.asContext(client: KeisClient): SchoolContext {
