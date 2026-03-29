26b1aa4d5ea7f14ab8729efa3a5d5288fc1ac89f
Jimin Seo <aa090402@naver.com>
2026-03-29 13:21:59 +0900

    feat: reactor

--- src/main/kotlin/com/github/mrjimin/keis/api/context/SchoolContext.kt
+++ src/main/kotlin/com/github/mrjimin/keis/api/context/SchoolContext.kt
@@ -1,7 +1,7 @@
 package com.github.mrjimin.keis.api.context
 
 import com.github.mrjimin.keis.KeisClient
-import com.github.mrjimin.keis.api.dsl.TimetableQueryBuilder
+import com.github.mrjimin.keis.api.dsl.builder.TimetableQueryBuilder
 import com.github.mrjimin.keis.api.timetable
 import com.github.mrjimin.keis.model.domain.School
 import com.github.mrjimin.keis.model.domain.Timetable
