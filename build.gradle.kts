b3736cc9d48d7617c72094b8d76e46a138695d83
Jimin Seo <aa090402@naver.com>
2026-03-31 20:06:25 +0900

    feat: 버전관리 방법 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.3.20"
-    id("io.ktor.plugin") version "3.4.1"
-    kotlin("plugin.serialization") version "2.3.20"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.github.mrjimin.keis"
@@ -14,25 +11,24 @@ application {
     mainClass = "com.github.mrjimin.keis.MainKt"
 }
 
-kotlin {
-    jvmToolchain(21)
-}
-
 dependencies {
-    implementation("io.ktor:ktor-client-core")
-    implementation("io.ktor:ktor-client-cio")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
 
-    implementation("io.ktor:ktor-client-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.logback.classic)
 
-    testImplementation("io.ktor:ktor-client-mock")
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
     ignoreFailures = true
+}
+
+kotlin {
+    jvmToolchain(21)
 }
