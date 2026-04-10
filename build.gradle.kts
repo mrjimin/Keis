752add9f246cd2cbb21019d412165dbc68b6cbf6
Jimin Seo <aa090402@naver.com>
2026-04-10 15:19:49 +0900

    feat: 2.0.1, java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.mrjimin.keis"
-version = "2.0.0"
+version = "2.0.1"
 
 subprojects {
 
@@ -23,7 +23,7 @@ subprojects {
     }
 
     kotlin {
-        jvmToolchain(25)
+        jvmToolchain(21)
     }
 
     tasks.test {
