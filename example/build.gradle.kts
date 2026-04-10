cbf9d48f3d9f0016910f6cf53fc19ed193d666db
Jimin Seo <aa090402@naver.com>
2026-04-10 15:37:17 +0900

    feat: add example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(project(":keis-core"))
     implementation(project(":keis-ktor"))
     implementation(project(":keis-http4k"))
+    implementation(project(":keis-spring"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
 
---
+++ example/src/main/kotlin/com/github/mrjimin/keis/example/RestClientExample.kt
@@ -0,0 +1,20 @@
+package com.github.mrjimin.keis.example
+
+import com.github.mrjimin.keis.core.api.school.schoolContext
+import com.github.mrjimin.keis.example.common.mealExample
+import com.github.mrjimin.keis.example.common.scheduleExample
+import com.github.mrjimin.keis.example.common.schoolExample
+import com.github.mrjimin.keis.example.common.timetableExample
+import com.github.mrjimin.keis.spring.keisRestClient
+
+suspend fun main() {
+    // keisRestClient(key: String, client: HttpHandler)
+    val client = keisRestClient("YOUR_API_KEY")
+    val context = client.schoolContext("우석고") ?: return
+
+    schoolExample(client)
+
+    mealExample(context)
+    scheduleExample(context)
+    timetableExample(context)
+}
