de195c3cad4bbbf9889cac82b835ab4060fa6049
Jimin Seo <aa090402@naver.com>
2026-05-16 10:16:02 +0900

    feat: RestClientEngine.kt add CoroutineContext

--- spring-boot/src/main/kotlin/com/github/mrjimin/keis/spring/RestClientEngine.kt
+++ spring-boot/src/main/kotlin/com/github/mrjimin/keis/spring/RestClientEngine.kt
@@ -7,13 +7,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.toEntity
+import kotlin.coroutines.CoroutineContext
 
 class RestClientEngine(
-    private val client: RestClient
+    private val client: RestClient,
+    private val coroutineContext: CoroutineContext
 ): HttpEngine {
 
     override suspend fun get(url: String, query: Map<String, String>): HttpResponse =
-        withContext(Dispatchers.IO) {
+        withContext(coroutineContext) {
             val response = client.get()
                 .uri(buildUrlWithQuery(url, query))
                 .retrieve()
@@ -23,5 +25,5 @@ class RestClientEngine(
         }
 }
 
-fun keisRestClient(key: String, client: RestClient = defaultRestClient): KeisClient =
-    KeisClient(key, RestClientEngine(client))
+fun keisRestClient(key: String, client: RestClient = defaultRestClient, coroutineContext: CoroutineContext = Dispatchers.IO): KeisClient =
+    KeisClient(key, RestClientEngine(client, coroutineContext))
