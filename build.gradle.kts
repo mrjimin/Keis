6444004885c99568442b1e98cdf5a622445ba247
mrjimin <aa090402@naver.com>
2026-09-10 19:56:07 +0900

    feat: 3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.mrjimin.keis"
-version = "3.1.1"
+version = "3.2.0"
 
 repositories {
     mavenCentral()
@@ -47,7 +47,7 @@ subprojects {
     }
 
     kotlin {
-        jvmToolchain(25)
+        jvmToolchain(21)
     }
 
     tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "Keis"
+rootProject.name = "keis"
 
 dependencyResolutionManagement {
     repositories {
